bffdce084fb4e7e8793c90c7904f35f8e84cb5b8
wujinke <295687784@qq.com>
2015-09-17 13:02:58 +0800

    add README & core

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-### Angular-ProgressBar2
+## Angular-ProgressBar2
+### Using Example:
+
+&lt;progressbar2
+    value="0.6"
+    type="'circle|info'"
+    animate="true"
+    options="userOptions"&gt;
+&lt;/progressbar2&gt;
+
+### Description
+
+#### value {Number}
+
+The value must be between 0 and 1.
+
+#### type {String}
+
+format: 'shapeType|colorType'
+
+* shapeType: 'circle|line'
+* colorType: 'success|info|warning|danger' `Like bootstrap progressbar type`
+
+#### animate {Boolean}
+
+#### options {Object}
+
+`Reference [progressbar.js](https://github.com/kimmobrunfeldt/progressbar.js) to config it.`
--- src/angular-progressbar2.js
+++ src/angular-progressbar2.js
@@ -3,86 +3,101 @@
  * @author jkwu
  */
 (function(ProgressBar) {
-var 
-    // default options for progress bar
-    DEFAULTS = {
-        strokeWidth: 10,
-        trailWidth: 10,
-        easing: 'linear',
-        trailColor: '#f0f0f0',
-        text: {
-            value: '0%',
-            style: {
+  
+  // default options for progress bar
+  var DEFAULTS = {
+    strokeWidth: 10,
+    trailWidth: 10,
+    easing: 'linear',
+    trailColor: '#f0f0f0',
+    text: {
+      value: '0%',
+      style: {
 
-            }
-        },
-        svgStyle: {
-            width: '100%',
-            height: '100%'
-        },
-        duration: 500,
-        step: function(state, bar) {
-            bar.setText((bar.value() * 100).toFixed(0) + '%');
-        }
+      }
     },
+    svgStyle: {
+      width: '100%',
+      height: '100%'
+    },
+    duration: 500,
+    step: function(state, bar) {
+      bar.setText((bar.value() * 100).toFixed(0) + '%');
+    }
+  },
 
-    // progress bar type like 'bootstrap'
-    BAR_TYPES = {
-        success: {
-            color: '#5cb85c',
+  // progress bar type like 'bootstrap'
+  var BAR_TYPES = {
+    success: {
+      color: '#5cb85c',
+    },
+    info: {
+      color: '#5bc0de',
+    },
+    warning: {
+      color: '#f0ad4e',
+    },
+    danger: {
+      color: '#d9534f'
+    }
+  };
+
+  angular.module('progressbar2', [])
+
+  /**
+   * @param {Float} value Must be '<=' 1 and '>=' 0
+   * @param {String} type Bar shape and color type, like 'circle|info' 
+   * @param {Boolean} animate When show animation or not
+   * @param {options} options User options
+   * @example
+   * <progressbar2
+   *   value="0.6"
+   *   type="'circle|info'"
+   *   animate="true"
+   *   options="userOptions">
+   * </progressbar2>
+   */
+  .directive('progressbar2', [function() {
+      return {
+        restrict: 'AE',
+        replace: true,
+        scope: {
+          value  : '=value',
+          type   : '=type',
+          animate: '=animate',
+          options: '=options'
         },
-        info: {
-            color: '#5bc0de',
-        },
-        warning: {
-            color: '#f0ad4e',
-        },
-        danger: {
-            color: '#d9534f'
+        templateUrl: 'tpl/progressbar2.html',
+        link: function(scope, element, attrs) {
+
+          var 
+            types = scope.type.split('|'),
+            funcName = types[0],
+            barType = types[1],
+            setType = scope.animate ? 'animate' : 'set',
+            options = scope.options || {},
+            progressBar;
+
+          // merge options with DEFAULTS
+          options = angular.extend(DEFAULTS, BAR_TYPES[barType], options);
+
+          // translate function name of ProgressBar,
+          // Circle, Line
+          funcName = funcName.charAt(0).toUpperCase() + funcName.substring(1);
+
+          // Initilize
+          progressBar = new ProgressBar[funcName](element[0], options);
+
+          // setting value
+          progressBar[setType](scope.value);
         }
-    };
+      };
+  }])
 
-    angular.module('progressbar2', []).directive('progressbar2', [function() {
-        return {
-            restrict: 'AE',
-            replace: true,
-            scope: {
-            // {Number} 0.xx
-            value: '=value',
-            // {String} 'circle|success', 'line|info'
-            type: '=type',
-            //
-            animate: '=animate',
-            // {Object}
-            options: '=options'
-            },
-            templateUrl: 'tpl/progressbar2.html',
-            link: function(scope, element, attrs) {
-                var types = scope.type.split('|'),
-                funcName = types[0],
-                barType = types[1],
-                setType = scope.animate ? 'animate' : 'set',
-                options,
-                progressBar;
-
-                // merge options with DEFAULTS
-                options = angular.extend(DEFAULTS, BAR_TYPES[barType], scope.options);
-
-                // translate function name of ProgressBar,
-                // Circle, Line
-                funcName = funcName.charAt(0).toUpperCase() + funcName.substring(1);
-
-                // Initilize
-                progressBar = new ProgressBar[funcName](element[0], options);
-
-                // setting value
-                progressBar[setType](scope.value);
-            }
-        };
-    }])
-
-    .run(['$templateCache', function($templateCache) {
-        $templateCache.put('tpl/progressbar2.html', '<div class="progressbar2"></div>');
-    }]);
+  .run(['$templateCache', function($templateCache) {
+    $templateCache.put('tpl/progressbar2.html', 
+      '<div class="progressbar2"></div>'
+    );
+  }]);
 
 })(ProgressBar);
